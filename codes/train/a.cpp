14bdf4e8eb2fd195ad82d13510db15f0fe4e808a
Ubiratan Neto <ubiratanneto37@hotmail.com>
2019-08-27 19:00:01 -0300

    test ssh

--- codes/train/a.cpp
+++ codes/train/a.cpp
@@ -27,57 +27,98 @@ int gcd(int a, int b){
   else return gcd(b%a, a);
 }
 
-struct Manacher{
-  
-  vector<int> d_e, d_o;
-  Manacher(){}
-  
-  Manacher(int n){
-    d_e.resize(n);
-    d_o.resize(n);
-  }
-  
-  void calculate(string & s){
-    
-    int n = d_e.size();
-    //calculate for odd
-    for (int i = 0, l = 0, r = -1; i < n; i++) {
-      d_o[i] = (i > r) ? 1 : min(d_o[l + r - i], r - i + 1);
-      while(i - d_o[i] >= 0 and i + d_o[i] < n and s[i-d_o[i]] == s[i+d_o[i]]) d_o[i]++;
-      if(i + d_o[i]-1 > r){
-        l = i-d_o[i]+1;
-        r = i+d_o[i]-1;
-      }
-    }
-    
-    //calculate for even
-    for (int i = 0, l = 0, r = -1; i < n; i++) {
-      d_e[i] = (i > r) ? 0 : min(d_e[l + r - i + 1], r - i + 1);
-      while(i - d_e[i] - 1 >= 0 and i + d_e[i] < n and s[i-d_e[i]-1] == s[i+d_e[i]]) d_e[i]++;
-      if(i + d_e[i]-1 > r){
-        l = i-d_e[i];
-        r = i+d_e[i]-1;
-      }
-    }
-    
-  }
-  
+struct node {
+  int fst, lst, sum;
+  node(){}
+  node(int fst, int lst, int sum) : fst(fst), lst(lst), sum(sum) {}
 };
+
+node st[812345];
+int v[812345];
+
+node merge(node a, node b){
+  return node(0,0, a.sum + b.sum);
+}
+
+void build(int cur, int l, int r){
+  if(l == r){
+    st[cur] = node(0,0,v[l]);
+    return;
+  }
+  
+  int m = (l+r)>>1;
+  
+  build(2*cur, l, m);
+  build(2*cur+1, m+1, r);
+  
+  st[cur] = merge(st[2*cur], st[2*cur+1]);
+}
+
+void propagate(int cur, int l, int r){
+  if(st[cur].fst == 0) return;
+  
+  st[cur].sum += ((st[cur].fst + st[cur].lst)*(r-l+1))/2;
+  
+  if(l != r){
+    int m = (l+r)>>1;
+    
+    int step = (st[cur].lst - st[cur].fst)/(r-l);
+    
+    st[2*cur].fst += st[cur].fst;
+    st[2*cur].lst += st[cur].fst + (m-l)*step;
+    st[2*cur+1].fst += st[cur].fst + (m-l+1)*step;
+    st[2*cur+1].lst += st[cur].fst + (r-l)*step;
+    
+  }
+  
+  st[cur].fst = st[cur].lst = 0;
+}
+
+void update(int cur, int l, int r, int a, int b){
+  propagate(cur, l, r);
+  if(b < l or r < a) return;
+  if(a <= l and r <= b){
+    //lazy stuff
+    st[cur].fst = (l-a+1);
+    st[cur].lst = (r-a+1);
+    propagate(cur, l, r);
+    return;
+  }
+  
+  int m = (l+r)>>1;
+  
+  update(2*cur, l, m, a, b);
+  update(2*cur+1, m+1, r, a, b);
+  
+  st[cur] = merge(st[2*cur], st[2*cur+1]);
+}
+
+node query(int cur, int l, int r, int a, int b){
+  propagate(cur, l, r);
+  if(b < l or r < a) return node(0,0,0);
+  if(a <= l and r <= b){
+    return st[cur];
+  }
+  
+  int m = (l+r)>>1;
+  
+  node lft = query(2*cur, l, m, a, b);
+  node rgt = query(2*cur+1, m+1, r, a, b);
+  return merge(lft, rgt);
+}
 
 int32_t main(){
   DESYNC;
-  int n;
-  cin >> n;
-  string s;
-  cin >> s;
-  Manacher m(n);
-  m.calculate(s);
-  int ans = 0;
-  for(int i=0; i<n; i++){
-    ans = max(ans, m.d_o[i]*2-1);
-    ans = max(ans, m.d_e[i]*2);
+  int n,q;
+  cin >> n >> q;
+  for(int i=0; i<n; i++) cin >> v[i];
+  build(1, 0, n-1);
+  while(q--){
+    int t, l, r;
+    cin >> t >> l >> r;
+    if(t == 1) update(1, 0, n-1, l-1, r-1);
+    else cout << query(1, 0, n-1, l-1, r-1).sum << endl;
   }
-  cout << ans << endl;
 }
 
 
