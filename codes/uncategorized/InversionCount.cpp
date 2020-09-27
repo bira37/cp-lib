7a650509552822c01d2c44cec0a2f0bcd3289364
Ubiratan Neto <ubiratanneto37@gmail.com>
2020-09-27 20:11:14 -0300

    feat: substitute slow mergesort

--- codes/uncategorized/InversionCount.cpp
+++ codes/uncategorized/InversionCount.cpp
@@ -1,25 +1,29 @@
-/* Author: Ubiratan Correia Barbosa Neto
- * Inversion Count with Merge Sort
- */
-
-int mergesort_count(vector<int>& v) {
-  vector<int> a, b;
-  if (v.size() == 1) return 0;
-  for (int i = 0; i < v.size() / 2; i++) a.push_back(v[i]);
-  for (int i = v.size() / 2; i < v.size(); i++) b.push_back(v[i]);
+namespace Sorting {
+ 
+/* Merge Sort + Inversion Count */
+int merge(vector<int> &a, vector<int> &b, int l, int mid, int r) {
   int ans = 0;
-  ans += mergesort_count(a);
-  ans += mergesort_count(b);
-  a.push_back(LLONG_MAX);
-  b.push_back(LLONG_MAX);
-  int x = 0, y = 0;
-  for (int i = 0; i < v.size(); i++) {
-    if (a[x] <= b[y]) {
-      v[i] = a[x++];
+  int k = mid + 1;
+  for (int i = l; i <= r; i++) {
+    if (l <= mid && (k > r || a[l] <= a[k])) {
+      b[i] = a[l++];
     } else {
-      v[i] = b[y++];
-      ans += a.size() - x - 1;
+      ans += mid - l + 1;
+      b[i] = a[k++];
     }
   }
   return ans;
 }
+int merge_sort(vector<int> &a, vector<int> &b, int l, int r) {
+  if (l == r) return 0;
+  int mid = l + r >> 1;
+  return merge_sort(b, a, l, mid) + merge_sort(b, a, mid + 1, r) +
+         merge(a, b, l, mid, r);
+}
+int MergeSort(vector<int> &a) {
+  vector<int> b = a;
+  return merge_sort(b, a, 0, a.size() - 1);
+}
+/* Merge Sort + Inversion Count */
+ 
+}  // namespace Sorting
