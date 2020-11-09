74b5cf73e2677907d4678840bb399dd6e030d1f3
Ubiratan Neto <ubiratanneto37@gmail.com>
2020-11-09 19:40:40 -0300

    feat: add ntt from kactl

---
+++ codes/math_nt/NTT-Kactl.cpp
@@ -0,0 +1,58 @@
+/**
+ * Obs: this is a fork from kactl 
+ * Author: chilli
+ * Date: 2019-04-16
+ * License: CC0
+ * Source: based on KACTL's FFT
+ * Description: ntt(a) computes $\hat f(k) = \sum_x a[x] g^{xk}$ for all $k$, where $g=\text{root}^{(mod-1)/N}$.
+ * N must be a power of 2.
+ * Useful for convolution modulo specific nice primes of the form $2^a b+1$,
+ * where the convolution result has size at most $2^a$. For arbitrary modulo, see FFTMod.
+   \texttt{conv(a, b) = c}, where $c[x] = \sum a[i]b[x-i]$.
+   For manual convolution: NTT the inputs, multiply
+   pointwise, divide by n, reverse(start+1, end), NTT back.
+ * Inputs must be in [0, mod).
+ * Time: O(N \log N)
+ * Status: stress-tested
+ * Source: https://github.com/kth-competitive-programming/kactl/blob/master/content/numerical/NumberTheoreticTransform.h
+ */
+
+namespace NTT {
+const ll mod = (119 << 23) + 1, root = 62;  // = 998244353
+// For p < 2^30 there is also e.g. 5 << 25, 7 << 26, 479 << 21
+// and 483 << 21 (same root). The last two are > 10^9.
+typedef vector<ll> vl;
+int sz(const vl &x) { return x.size(); }
+void ntt(vl &a) {
+  int n = sz(a), L = 31 - __builtin_clz(n);
+  static vl rt(2, 1);
+  for (static int k = 2, s = 2; k < n; k *= 2, s++) {
+    rt.resize(n);
+    ll z[] = {1, exp(root, mod >> s)};
+    for (int i = k; i < 2 * k; i++) rt[i] = rt[i / 2] * z[i & 1] % mod;
+  }
+  vi rev(n);
+  for (int i = 0; i < n; i++) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
+  for (int i = 0; i < n; i++)
+    if (i < rev[i]) swap(a[i], a[rev[i]]);
+  for (int k = 1; k < n; k *= 2)
+    for (int i = 0; i < n; i += 2 * k)
+      for (int j = 0; j < k; j++) {
+        ll z = rt[j + k] * a[i + j + k] % mod, &ai = a[i + j];
+        a[i + j + k] = ai - z + (z > ai ? mod : 0);
+        ai += (ai + z >= mod ? z - mod : z);
+      }
+}
+vl Multiply(const vl &a, const vl &b) {
+  if (a.empty() || b.empty()) return {};
+  int s = sz(a) + sz(b) - 1, B = 32 - __builtin_clz(s), n = 1 << B;
+  int inv = exp(n, mod - 2);
+  vl L(a), R(b), out(n);
+  L.resize(n), R.resize(n);
+  ntt(L), ntt(R);
+  for (int i = 0; i < n; i++)
+    out[-i & (n - 1)] = (ll)L[i] * R[i] % mod * inv % mod;
+  ntt(out);
+  return {out.begin(), out.begin() + s};
+}
+}  // namespace NTT
